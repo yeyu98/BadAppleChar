68a09168a23160a927b4c1d875c50b9e12c1e24f
xiaohu <zhouyi@yx.com>
2023-09-07 20:37:05 +0800

    fix: iframe 属性

--- src/components/Star.tsx
+++ src/components/Star.tsx
@@ -8,7 +8,7 @@ function Star(props: Props) {
 
   return (
     <>
-      <iframe className='star' src="https://buttons.github.io/buttons.html#href=https%3A%2F%2Fgithub.com%2Fyeyu98%2FBadAppleChar&amp;title=&amp;aria-label=Star%20yeyu98%2FBadAppleChar%20on%20GitHub&amp;data-text=Star&amp;data-size=large" allowtransparency="true" scrolling="no" frameborder="0" style={{width: '63px', height: '28px', border: 'none'}} name="href=https%3A%2F%2Fgithub.com%2Fyeyu98%2FBadAppleChar&amp;title=&amp;aria-label=Star%20yeyu98%2FBadAppleChar%20on%20GitHub&amp;data-text=Star&amp;data-size=large"></iframe>
+      <iframe className='star' src="https://buttons.github.io/buttons.html#href=https%3A%2F%2Fgithub.com%2Fyeyu98%2FBadAppleChar&amp;title=&amp;aria-label=Star%20yeyu98%2FBadAppleChar%20on%20GitHub&amp;data-text=Star&amp;data-size=large" allowTransparency={true} scrolling="no" frameBorder={0} style={{width: '63px', height: '28px', border: 'none'}} name="href=https%3A%2F%2Fgithub.com%2Fyeyu98%2FBadAppleChar&amp;title=&amp;aria-label=Star%20yeyu98%2FBadAppleChar%20on%20GitHub&amp;data-text=Star&amp;data-size=large"></iframe>
     </>
   )
 }
